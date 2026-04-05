71e47d2a6750f35cfb0af7069cf4ad38b7d7a59f
RanderDLemos <162617589+RanderDLemos@users.noreply.github.com>
2026-04-05 19:22:33 -0300

    Add SistemaOperacional class

---
+++ br.inatel.cdg/compraPC/SistemaOperacional.java
@@ -0,0 +1,22 @@
+package br.inatel.cdg.compraPC;
+
+public class SistemaOperacional {
+
+    String nome;
+    int tipo;
+
+    public SistemaOperacional(String nome, int tipo) {
+
+        this.nome = nome;
+        this.tipo = tipo;
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+}
