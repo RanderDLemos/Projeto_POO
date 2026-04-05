2c63be01d1ac9ca69673635cb1ae610b43356b3c
RanderDLemos <162617589+RanderDLemos@users.noreply.github.com>
2026-04-05 19:23:04 -0300

    Add ProcessarPedido class

---
+++ br.inatel.cdg/Pedido/ProcessarPedido.java
@@ -0,0 +1,9 @@
+package br.inatel.cdg.Pedido;
+
+public class ProcessarPedido {
+
+    public static void exibirTotal(float total){
+        System.out.println("Total da compra: R$" + total);
+    }
+
+}
