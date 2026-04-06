229d34000eeeee18015c8c9f4f61348c2891ff9f
RanderDLemos <162617589+RanderDLemos@users.noreply.github.com>
2026-04-06 11:19:17 -0300

    Add promotional options and selection for computers

--- br.inatel.cdg/compraPC/Computador.java
+++ br.inatel.cdg/compraPC/Computador.java
@@ -45,6 +45,73 @@ public class Computador {
         if (i >= 0 && i < hb.length) {
             this.hb[i] = new HardwareBasico(nome, capacidade);
         }
+    }
+    public static void opcoesPromo(int opcao, int matricula){
 
+
+        if(opcao == -1){
+            System.out.println("Confira as promocoes disponiveis para voce: ");
+            System.out.println("[1] primeira promo: ");
+            System.out.println("- Marca: Apple");
+            System.out.println("- Preço: " + matricula);
+            System.out.println("- Pentium Core i3 (2200 Mhz)");
+            System.out.println("- 8 Gb de RAM");
+            System.out.println("- 500 Gb de HD");
+            System.out.println("- Sistema Operacional: macOS Sequoia (64)");
+            System.out.println("-----------------------------------------------------");
+            System.out.println("[2] primeira promo: ");
+            System.out.println("- Marca: Samsung");
+            System.out.println("- Preço: " + (1234 + matricula));
+            System.out.println("- Pentium Core i5 (3370 Mhz)");
+            System.out.println("- 16 Gb de RAM");
+            System.out.println("- 1 Tb de HD");
+            System.out.println("- Sistema Operacional: Windows 8 (64)");
+            System.out.println("-----------------------------------------------------");
+            System.out.println("[3] primeira promo: ");
+            System.out.println("- Marca: Dell");
+            System.out.println("- Preço: " + (5678 + matricula));
+            System.out.println("- Pentium Core i7 (4500 Mhz)");
+            System.out.println("- 32 Gb de RAM");
+            System.out.println("- 2 Tb de HD");
+            System.out.println("- Sistema Operacional: Windows 10 (64)");
+            System.out.println("-----------------------------------------------------");
+            System.out.println("[0] caso queira sair");
+            System.out.println("Qual opcao deseja selecionar?: ");
+        }else{
+            System.out.println("Deseja selecionar mais alguma opcao?");
+            System.out.println("[1] primeira promo");
+            System.out.println("[2] primeira promo");
+            System.out.println("[3] primeira promo");
+            System.out.println("[0] caso queira sair");
+        }
+
+    }
+
+    public static Computador opcaoEscolhida(int opcao, int matricula){
+        Computador tempPC = null;
+        switch (opcao){
+            case 1:
+                tempPC = new Computador("Apple", matricula, "macOS Sequoia", 64);
+                tempPC.addHardware(0, "Pentium Core i3", 2200);
+                tempPC.addHardware(1, "RAM", 8);
+                tempPC.addHardware(2, "HD", 500);
+                tempPC.addMemoriaUSB(new MemoriaUSB("Pendrive", 16));
+                break;
+            case 2:
+                tempPC = new Computador("Samsung", 1234 + matricula, "Windows 8", 64);
+                tempPC.addHardware(0, "Pentium Core i5", 3370);
+                tempPC.addHardware(1, "RAM", 16);
+                tempPC.addHardware(2, "HD", 1000);
+                tempPC.addMemoriaUSB(new MemoriaUSB("Pendrive", 32));
+                break;
+            case 3:
+                tempPC = new Computador("Dell", 5678 + matricula, "Windows 10", 64);
+                tempPC.addHardware(0, "Pentium Core i7", 4500);
+                tempPC.addHardware(1, "RAM", 32);
+                tempPC.addHardware(2, "HD", 2000);
+                tempPC.addMemoriaUSB(new MemoriaUSB("HD Externo", 1000));
+                break;
+        }
+        return tempPC;
     }
 }
